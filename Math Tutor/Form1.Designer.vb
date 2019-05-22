e64ffc48a712921cc1aa9283a492f6512092036c
Cristian Benitez <31877340+cbeni108@users.noreply.github.com>
2019-05-21 22:20:10 -0500

    Add files via upload

---
+++ Math Tutor/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShowAnswer = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnShowAnswer
+        '
+        Me.btnShowAnswer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnShowAnswer.Location = New System.Drawing.Point(89, 204)
+        Me.btnShowAnswer.Name = "btnShowAnswer"
+        Me.btnShowAnswer.Size = New System.Drawing.Size(157, 52)
+        Me.btnShowAnswer.TabIndex = 0
+        Me.btnShowAnswer.Text = "Show the answer!"
+        Me.btnShowAnswer.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(420, 204)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(104, 52)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Show Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(285, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Let us take the math application 18 + 64 = ?"
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(417, 49)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(88, 17)
+        Me.lblAnswer.TabIndex = 3
+        Me.lblAnswer.Text = "18 + 64 = 82"
+        Me.lblAnswer.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowAnswer)
+        Me.Name = "Form1"
+        Me.Text = "Math Tutor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnShowAnswer As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblAnswer As Label
+End Class
---
+++ Math Tutor/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub BtnShowAnswer_Click(sender As Object, e As EventArgs) Handles btnShowAnswer.Click
+        'Make answer visible'
+        lblAnswer.Visible = True
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form'
+        Me.Close()
+
+    End Sub
+End Class
